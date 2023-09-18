5be17801c90b4251e6a072caf2d07c1de4e11df9
Agostina <75433684+Agostina-mr@users.noreply.github.com>
2023-09-18 17:31:56 +0200

    add data

--- Assignment1/src/scripts/postdata.js
+++ Assignment1/src/scripts/postdata.js
@@ -13,18 +13,38 @@ async function postData(data = {}) {
 }
 
 
-
 function submitData() {
-  const formData = new FormData(document.getElementById('data-post'));
-  const jsonData = {}
-  formData.forEach((value, key) => {
-    jsonData[key] = value;
-  })
+  let jsonData = [{"type": "temperature",
+  "time": readFromForm('temperature_time'),
+  "place": readFromForm('temperature_place'),
+  "value": readFromForm('temperature_value'),
+  "unit": readFromForm('temperature_unit')},
+ {"type": "precipitation",
+  "time": readFromForm('prec_time'),
+  "place": readFromForm('prec_place'),
+  "value": readFromForm('prec_value'),
+  "unit": readFromForm('prec_unit'),
+  "precipitation_type": readFromForm('p_prec_type')},
+ {"type": "wind speed",
+  "time": readFromForm('wind_speed_time'),
+  "place": readFromForm('wind_speed_place'),
+  "value": readFromForm('wind_speed_value'),
+  "unit": readFromForm('wind_speed_unit'),
+  "direction": readFromForm('wind_speed_direction')},
+{"type": "cloud coverage",
+  "time": readFromForm('cloud_coverage_time'),
+  "place": readFromForm('cloud_coverage_place'),
+  "value": readFromForm('cloud_coverage_value'),
+  "unit": readFromForm('cloud_coverage_unit')}]
 
-  postData(jsonData).then((data) => {
-    console.log(data)
+  postData(jsonData).then((response) => {
+    console.log("Response = " + response)
   })
 }
 
 let submitButton = document.getElementById('submit-button')
 submitButton.addEventListener("click", () => submitData())
+
+function readFromForm(element){
+  return (document.getElementById(element)).value
+}
